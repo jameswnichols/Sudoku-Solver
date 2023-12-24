812b4a815ed60d0ff7e6cc45eb1a322f09418de5
James Nichols <jnichols.tigley@gmail.com>
2023-12-24 12:28:32 +0000

    Work on square scanning.

--- solver.py
+++ solver.py
@@ -46,6 +46,7 @@ class Board:
     def __init__(self, width : int, height : int):
         self.board = {}
         self.subGrids = {}
+        self.unscannedPositions = []
         self.width, self.height = width, height
 
     def _generatePreSubgridHashes(self, numberLocations : dict[Vector2, int]):
@@ -58,10 +59,33 @@ class Board:
                 subgridNumbers[subGridVector].add(number)
         
         return {k : list(set(range(1, 10)) - v) for k, v in subgridNumbers.items()}
-    
+
+    def _getAdjecentToPosition(self, position : Vector2):
+        possibleLocations = set()
+        for y in range(0, self.height):
+            possibleLocations.add(Vector2(position.x, y))
+        for x in range(0, self.width):
+            possibleLocations.add(Vector2(x, position.y))
+        possibleLocations.remove(position)
+        return list(possibleLocations)
+
+    def scanSquares(self):
+        while self.unscannedPositions:
+            position = self.unscannedPositions.pop()
+            squareNumber = self.board[position].number
+            possibleLocations = self._getAdjecentToPosition(position)
+            for location in possibleLocations:
+                if squareNumber in self.board[location].notedNumbers:
+                    self.board[location].notedNumbers.remove(squareNumber)
+
+    def setNumber(self, position : Vector2, number : int):
+        self.board[position].number = number
+        self.unscannedPositions.append(position)
+
     def setup(self, numberLocations : dict[Vector2, int] = {}):
         self.board = {}
         self.subGrids = {}
+        self.unscannedPositions ={} #list(numberLocations.keys())
 
         #Generates a list of possible numbers that can be in each square in each subgrid, minus pre-supplied numbers.
         notedNumberLookup = self._generatePreSubgridHashes(numberLocations)
@@ -79,13 +103,16 @@ class Board:
 
                 #If the position is in the pre-supplied numbers, then use that number instead.
                 squareNumber = numberLocations[positionVector] if positionVector in numberLocations else None
-                self.board[positionVector] = Square(squareNumber, self.subGrids[subGridVector])
+                self.board[positionVector] = Square(None, self.subGrids[subGridVector])
+
+                self.setNumber(positionVector, squareNumber)
 
                 #All possible numbers can be in every box unless that subgrid has been added to the lookup table.
-                notedNumbers = [range(1, 10)] if subGridVector not in notedNumberLookup else notedNumberLookup[subGridVector]
-                self.board[positionVector] = notedNumbers
+                self.board[positionVector].notedNumbers = [i for i in range(1, 10)] if subGridVector not in notedNumberLookup else notedNumberLookup[subGridVector]
 
                 self.subGrids[subGridVector].addSquare(self.board[positionVector])
+        
+        self.scanSquares()
 
 sudokuBoard = Board(9, 9)
 sudokuBoard.setup(numberLocations={Vector2(4, 4) : 1})
